f436cfca9b618d5239b00aa4e0c902469a80463e
Agushadad <agushadad@gmail.com>
2021-04-16 14:28:19 -0300

    Buenas practicas

---
+++ Proyecto DeliverEat/Producto/Codigo_Fuente/isw_deliveryeat/lib/homepage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Center(
+        child: Text("TestHomePage"),
+      )),
+    );
+  }
+}
--- Proyecto DeliverEat/Producto/Codigo_Fuente/isw_deliveryeat/lib/splash_screen.dart
+++ Proyecto DeliverEat/Producto/Codigo_Fuente/isw_deliveryeat/lib/splash_screen.dart
@@ -1,21 +1,21 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:isw_deliveryeat/screens/home/home_screen.dart';
+import 'package:isw_deliveryeat/homepage.dart';
 
-class splashScreen extends StatefulWidget {
+class SplashScreen extends StatefulWidget {
   @override
-  _splashScreenState createState() => _splashScreenState();
+  SplashScreenState createState() => SplashScreenState();
 }
 
-class _splashScreenState extends State<splashScreen> {
-
+class SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    Timer(Duration(seconds: 3),(){
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>HomeScreen()));
+    Timer(Duration(seconds: 1), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => HomePage()));
     });
   }
 
@@ -24,11 +24,10 @@ class _splashScreenState extends State<splashScreen> {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
-        child: SpinKitDoubleBounce(
-          color: Colors.white,
-          size: 80.0,
-        )
-      ),
+          child: SpinKitDoubleBounce(
+        color: Colors.white,
+        size: 80.0,
+      )),
     );
   }
 }
